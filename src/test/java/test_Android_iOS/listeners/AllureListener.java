ea714ae04134f202c6dfc8914832d1a5e8f49498
Alexandr <alexandr.bogachev@gmail.com>
2022-04-27 11:23:39 +0300

    Создал клон проекта для пушинга на GitLab

---
+++ src/test/java/test_Android_iOS/listeners/AllureListener.java
@@ -0,0 +1,34 @@
+package test_Android_iOS.listeners;
+
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class AllureListener implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        String testName = result.getName();
+        if (result.getName().contains("Screenshot")) {
+            String filePath = "diff/diff_" + testName + ".png";
+            try {
+                saveScreenshot(Files.readAllBytes(Paths.get(filePath)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            saveScreenshot(Selenide.screenshot(OutputType.BYTES));
+        }
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot (byte[] screenshot) {
+        return screenshot;
+    }
+}
